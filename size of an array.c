ee34e70b3ed2382650333eeb28624fbd974776eb
EmaMaker <emamaker0@gmail.com>
2018-12-27 21:55:38 +0100

    A

--- size of an array.c
+++
@@ -1,4 +0,0 @@
-	int ciao[3];
-
-	printf("%ld", sizeof(ciao) / sizeof(*ciao)); //size in bytes of the array divided by the size in bytes of the type (pointer to the array, or first array of the element)
-
