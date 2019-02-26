50b9f2a657a038168d3fe38cbd4bb4dc8ae558c7
EmaMaker <emamaker0@gmail.com>
2019-02-26 08:59:05 +0100

    Update timeout_scanf.c

--- timeout_scanf.c
+++ timeout_scanf.c
@@ -6,22 +6,30 @@
 #include <unistd.h>
 
 int main(void) {
+	//Value to be read
 	int value = 0;
+	
+	//Init the counter
 	struct timeval tmo;
 	fd_set readfds;
 
+	//Debug message
 	printf("Enter a non-zero number: ");
+	
 	fflush(stdout);
 
 	FD_ZERO(&readfds);
 	FD_SET(0, &readfds);
-	tmo.tv_sec = 3;
-	tmo.tv_usec = 0;
+	//Set max. time
+	tmo.tv_sec = 3;	//seconds
+	tmo.tv_usec = 0; //microseconds
 
 	if (select(1, &readfds, NULL, NULL, &tmo) == 0) {
+		//What happens if user didn't give input
 		printf("User dont give input");
 		//return (1);
 	} else {
+		//Examine given input
 		scanf("%d", &value);
 		if (value != 0) {
 			printf("User input a number");
@@ -30,6 +38,7 @@ int main(void) {
 		}
 	}
 
+	//Goes one
 	printf("boh");
 
 	return (0);
